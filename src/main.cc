905571c990b1e5854fa5f971324d46872e8747fb
Johannes Schneider <johannes.schneider@student.hpi.de>
2017-11-07 11:17:13 +0100

    Added hello world js for further testing purposes

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,14 @@
 #include "node.h"
 
 int main(int argc, char* argv[]) {
-    printf("Hello Embedded Node.js!\n");
+
+    if (argc <= 1)
+    {
+        // TOOD: print usage
+        return 1;
+    }
+
+    printf("Hello World from embedding C++!\n");
     node::Start(argc, argv);
     return 0;
 }
